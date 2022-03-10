bdcdde8fe01dc3334164140ae97ba7f084686a18
Alfandi Safira <71958455+fandisafira@users.noreply.github.com>
2022-03-10 11:21:55 +0700

    Mengubah judul halaman pada path_admin.php.

--- admin/path_admin.php
+++ admin/path_admin.php
@@ -33,7 +33,7 @@ if (isset($_SESSION['login'])) {
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>CEGOR | Home</title>
+	<title>CEGOR | Path Admin</title>
 	<link rel="stylesheet" href="css/stylespath.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </head>
